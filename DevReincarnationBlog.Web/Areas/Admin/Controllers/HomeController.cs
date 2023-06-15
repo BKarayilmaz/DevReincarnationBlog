34af70cd23ce3f9cfbd9967908364fffca23980d
BKarayilmaz <berkankarayilmaz@gmail.com>
2023-06-15 12:07:51 +0300

    Routing And Admin Layout

---
+++ DevReincarnationBlog.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using DevReincarnationBlog.Service.Services.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevReincarnationBlog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class HomeController : Controller
+    {
+        private readonly IArticleService articleService;
+
+        public HomeController(IArticleService articleServices)
+        {
+            this.articleService = articleServices;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var articles = await articleService.GetAllArticlesAsync();
+            return View(articles);
+        }
+    }
+}
